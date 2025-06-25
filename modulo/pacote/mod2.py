1d8676cfb7176e0f52bbbdd0a7c5d76601a11b55
David <david.willyam23@gmail.com>
2025-06-25 18:01:06 -0300

    Adição da função de subtração
    A função de subtração está no módulo 2 (mod2).

--- modulo/pacote/mod2.py
+++ modulo/pacote/mod2.py
@@ -1,2 +1,5 @@
 def soma(num1, num2=1):
     return num1 + num2
+
+def subtracao(num1, num2=1):
+    return num1 - num2
--- modulo/principal.py
+++ modulo/principal.py
@@ -6,4 +6,6 @@ print(mod1.func_z())
 print(mod1.y)
 print(y)
 
-print(mod2.soma(3))
+print(f"Soma de 3 + 1 = {mod2.soma(3)}")
+print(f"Subtração de 3 - 1 = {mod2.subtracao(3)}")
+#Editei os prints pq achei que ficaria mais "legível" assim, mas mesmo sem eles dá pra entender.
